85bcf718668d951ab4f34ee823bec2f3a2174983
Ashok1328 <ashokkhatiwada1990@gmail.com>
2025-11-16 13:50:43 +0545

    Login Page

--- carriercenterapp/LoginPage.spec.js
+++ carriercenterapp/LoginPage.spec.js
@@ -1,7 +1,17 @@
-exports.LoginPage = class LoginPage
-{
-  constructor(page)
-  {
-    
+exports.LoginPage = class LoginPage {
+  constructor(page) {
+    this.usernameInput = "";
+    this.passwordInput = "";
+    this.loginBtn = "";
   }
-}
+
+  async gotoLoginpage() {
+    await this.page.goto("");
+  }
+
+  async login(username, password) {
+    await this.page.locator(this.usernameInput).fill(username);
+    await this.page.locator(this.passwordInput).fill(password);
+    await this.page.locator(this.loginBtn).click();
+  }
+};
